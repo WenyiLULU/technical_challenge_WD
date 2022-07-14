583ef4b4c4aae69c1577298dabd2c9806851dcbd
WenyiLULU <luwenyi1991@gmail.com>
2022-07-14 18:40:26 +0200

    correction of useEffect on Details

--- phone-cave-front/src/components/PhoneDetails.jsx
+++ phone-cave-front/src/components/PhoneDetails.jsx
@@ -7,13 +7,13 @@ const PhoneDetails = ({showDetails, phoneId}) => {
 
     const fetchPhone = async ()=>{
         const response = await axios.get(`http://localhost:5005/phones/${phoneId}`)
-        console.log(response.data)
+        console.log("phone data",response.data)
         setPhone(response.data)
         setFetching(false)
     }
     useEffect(()=>{
         fetchPhone()
-    },[])
+    },[phoneId])
     return ( 
         <div style={{display: showDetails, width:"60%", margin:"20 auto"}}>
             <div>Details of {phone.name}</div>
